78f2defbf232c3ce3dedbe7eb3b27f06e23a0380
Bede Kelly <bede.kelly@accenture.com>
2019-08-22 23:44:01 +0100

    Tune some bits for performance and get rid of unused styles

--- js/projects.js
+++ js/projects.js
@@ -4,35 +4,6 @@ import htm from './vendor/htm.module.js';
 const html = htm.bind(h);
 
 
-function collapse(section) {
-  section.classList.remove("expanded");
-  section.classList.add("collapsed");
-  for (let a of section.getElementsByClassName("project")) {
-    a.style.display = "none";
-  }
-}
-
-
-function expand(section) {
-  section.classList.add("expanded");
-  section.classList.remove("collapsed");
-  for (let a of section.getElementsByClassName("project")) {
-    a.style.display = "initial";
-  }
-}
-
-
-function toggleVisible(target) {
-  let section = target.parentElement;
-  if (section.classList.contains("expanded")) {
-    collapse(section);
-  } else {
-    expand(section);
-  }
-}
-
-
-
 function Project({project}) {
   const {title, description, url, image} = project;
 
